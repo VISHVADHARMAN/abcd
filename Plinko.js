ad3753efba981eb5afc908b79bbbdc985afa8db6
VISHVADHARMAN <67373113+VISHVADHARMAN@users.noreply.github.com>
2020-07-25 21:52:42 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,28 @@
+class Plinko{
+  constructor(x,y,r){
+    var options = {
+        isStatic: true
+        //restitution : 0.3,
+        //friction : 0.5,
+        //density : 1.2
+    }
+ this.x = x;
+ this.y = y;
+ this.r = r;
+ this.body = Bodies.circle(this.x,this.y,this.r/2,options);
+ World.add(world,this.body);
+}
+display(){
+  var pos = this.body.position;
+  push();
+  translate(pos.x,pos.y);
+  rectMode(CENTER);
+  strokeWeight(0);
+  fill("white");
+  ellipse(0,0,this.r);
+  
+  pop();
+
+}
+    
+}
